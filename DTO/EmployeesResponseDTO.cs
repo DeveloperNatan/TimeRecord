52234d2bbf932fc143659b9173af630e8fafc8b3
DeveloperNatan <natandesouza403@gmail.com>
2026-01-15 15:42:44 -0300

    Add DTO and refactoring

---
+++ DTO/EmployeesResponseDTO.cs
@@ -0,0 +1,10 @@
+namespace TimeRecord.Models
+{
+    public class EmployeeResponseDTO
+    {
+        public int MatriculaId { get; set; }
+        public string Nome { get; set; }
+        public string Cargo { get; set; }
+        public string Email { get; set; }
+    }
+}
